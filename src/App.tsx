91b2862d17b7de016839f310a2906b44d30140c5
Mohammad Alkhattab <mohammad.alkhattab77@gmail.com>
2024-03-08 14:00:57 +0200

    Ready to Deploy

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import EthereumInfo from "./component/EthereumInfo"
+import './App.css'
+function App() {
+  
+
+  return (
+    <>
+      <main className="main">
+        <EthereumInfo/>
+      </main>
+       
+    </>
+  )
+}
+
+export default App
